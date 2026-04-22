e47b3eaeaca0f21792cc865b3ea6850db1a3bae9
lourencomatias <lourencomatias0606@gmail.com>
2026-04-22 15:49:16 +0100

    Corrigi o prog21.c e o processFile

--- se2/ex1/lineprint.c
+++ se2/ex1/lineprint.c
@@ -2,16 +2,6 @@
 #include <string.h>
 #include "errors.h"
 
-/*
-1.2. Escreva a função: 
-
-int linePrintRaw( const char *line, void *context );
-destinada a ser passada no parâmetro action da função anterior para apresentar, em standard output, o
-conteúdo integral da linha de texto indicada em line.
-A função retorna 1. O parâmetro context não é usado neste exercício; existe para compatibilidade de
-assinatura, mas deve ser ignorado pelo código da função.
-*/
-
     //printf retorna o numero de caracteres, entao, se der print a alguma coisa(printf > 0)
 int linePrintRaw( const char *line, void *context ){  // retorna a logica da comparação, que é sempre 0 ou 1
     return (printf("%s", line) > 0); 
--- se2/ex1/prog21.c
+++ se2/ex1/prog21.c
@@ -7,20 +7,18 @@
 #define MAX_LINE_SIZE 512
 
 /*
-1.3. Escreva e teste um programa (com o nome prog21.c e executável prog21) para demostração das
+Escreva e teste um programa (com o nome prog21.c e executável prog21) para demostração das
 funções anteriores. O programa recebe, em argumento de linha de comando, o nome de um ficheiro e
 apresenta, em standard output, o seu conteúdo integral
 */
-int main(int argc, char *argv[]){
 
-
-    // Verificação das entradas
-    if (argc != 1) {
-        fatal_error("Erro nas entradas: ./prog21 <nome do ficheiro>\n");
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fatal_error("Uso: ./prog21 <nome do ficheiro>\n");
     }
+    
     char *ficheiro = argv[1];
-    char line[MAX_LINE_SIZE];
-    processFile(ficheiro,linePrintRaw(line,NULL), NULL);
+    processFile(ficheiro, linePrintRaw, NULL);
 
     return 0;
 }
