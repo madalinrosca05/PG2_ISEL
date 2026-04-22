e65dffa8cec2ccfc3b2378e2595f9e99237ed6bc
lourencomatias <lourencomatias0606@gmail.com>
2026-04-22 16:51:00 +0100

    book.c e book.h (3.1, 3.2, 3.3)

---
+++ se2/ex3/book.h
@@ -0,0 +1,27 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+// Dimensões baseadas na análise real de dados.csv
+#define MAX_BOOKS     350   // Superior às 296 linhas (livros) contadas
+#define MAX_TITLE     256   // Superior aos 200 carateres do título mais longo
+#define SIZE_ISBN     20    // Superior aos 10 carateres
+#define MAX_AUTHORS   128   // Superior aos 92 carateres do campo de autores
+#define MAX_PUB_NAME  64    // Superior aos 51 carateres do campo da editora
+
+typedef struct book {
+    char title[MAX_TITLE];
+    char isbn[SIZE_ISBN];
+    char authors[MAX_AUTHORS];
+    char publisher[MAX_PUB_NAME];
+} BookData;
+
+typedef struct {
+    BookData books[MAX_BOOKS];
+    BookData *refs[MAX_BOOKS]; 
+    int count; 
+} Collection;
+
+int fillBookData(BookData *b, const char *line);
+int collAddBook(const char *line, void *context);
+
+#endif
