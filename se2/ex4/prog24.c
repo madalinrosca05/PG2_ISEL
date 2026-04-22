95138587dd51038bf245441fca739dbb7e5f2e94
lourencomatias <lourencomatias0606@gmail.com>
2026-04-22 17:39:20 +0100

    comentários no prog24

--- se2/ex4/prog24.c
+++ se2/ex4/prog24.c
@@ -3,6 +3,7 @@
 #include "processFile.h"
 #include "errors.h"
 
+// Função para listar os livros ordenados por título
 void listBooks(Collection *col) {
     collSortTitle(col); 
     for (int i = 0; i < col->count; i++) {
@@ -17,11 +18,13 @@ int main(int argc, char *argv[]) {
         fatal_error("Uso: ./prog24 <ficheiro_de_dados>\n");
     }
 
+    // Inicializa a coleção e processa o ficheiro de dados
     Collection col = { .count = 0 };
     if (processFile(argv[1], collAddBook, &col) < 0) {
         fatal_error("Erro ao processar ficheiro.\n");
     }
-
+    
+    // Loop de comando para listar livros ou sair
     char command;
     while (printf("> "), scanf(" %c", &command) == 1) {
         if (command == 'q') break;
